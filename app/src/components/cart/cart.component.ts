9712df551e905373b8ca0157a744def8ced9053d
Alex <adahy.to@gmail.com>
2024-10-17 18:53:35 +0200

    few visual fixes

--- app/src/components/cart/cart.component.ts
+++ app/src/components/cart/cart.component.ts
@@ -2,11 +2,5 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 
 @Component({})
-export default class CartComponent extends Vue {
-
-  mounted (){
-    console.log('hello from app');
-  }
-  
-}
+export default class CartComponent extends Vue { }
 
