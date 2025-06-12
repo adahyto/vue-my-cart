4b9295243f87fae73c072db684b15377891a4050
Alex <adahy.to@gmail.com>
2025-06-12 22:36:35 +0200

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,6 +2,6 @@ const express = require("express");
 const serveStatic = require("serve-static");
 const port = process.env.PORT || 5001;
 app = express();
-app.use(serveStatic("/dist"));
+app.use(serveStatic(__dirname + "/dist"));
 app.listen(port);
 console.log("server started " + port);
